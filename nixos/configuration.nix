9ee5e388cafee5b8ee744e4827f764b4ef4715a5
kharf <kevinfritz210@gmail.com>
2023-06-27 17:21:54 +0200

    feat: add podman

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,6 +139,13 @@
     font-awesome
   ];
 
+  # Container
+  virtualisation = {
+    podman = {
+      enable = true;
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
