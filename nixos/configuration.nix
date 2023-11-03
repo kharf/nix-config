93754465fcb90909739ef66e016073846a1e538d
kharf <kevinfritz210@gmail.com>
2023-11-03 08:30:34 +0100

    feat: update pkgs and add step and dbeaver

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -39,6 +39,8 @@
       experimental-features = "nix-command flakes";
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
+      substituters = ["https://nix-gaming.cachix.org"];
+      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
     };
   };
 
@@ -212,6 +214,7 @@
      unstable.nil
      unstable.sops
      unstable.httpie
+     unstable.dbeaver
      # key remap (executed in zshrc)
      xorg.xmodmap
      # pictures
@@ -231,6 +234,7 @@
      _1password
      _1password-gui
      age
+     step-cli
      # system
      bottom
      killall
@@ -241,7 +245,7 @@
          killall picom
        '';   
        postCommands = ''
-         picom -b
+         exec picom -b
        '';   
      })
      # peripherals
