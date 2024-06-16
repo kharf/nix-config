032932c334168e5defbf4f0cfc3d8e4f40d4a2cb
kharf <kevinfritz210@gmail.com>
2024-06-16 17:21:44 +0200

    feat: update declcd to 0.22.10

--- pkgs/declcd/default.nix
+++ pkgs/declcd/default.nix
@@ -1,11 +1,11 @@
 { pkgs, stdenv, lib, installShellFiles }:
 pkgs.stdenv.mkDerivation rec {
   pname = "declcd";
-  version = "v0.22.9";
+  version = "v0.22.10";
   src = {
     x86_64-linux = pkgs.fetchurl {
       url = "https://github.com/kharf/${pname}/releases/download/${version}/${pname}_linux_x86_64.tar.gz";
-      hash = "sha256-3h5KIJNvBDI3/2JNGbfqIS+iac0yiC+ASjNIQJteulI=";
+      hash = "sha256-W75r1sOQAduJnMrYqAuVkXKDePaTSEajLzfVUgQAj2M=";
     };
   }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
