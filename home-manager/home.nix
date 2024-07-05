603b0f546f826351e2b87ff8012a1174bd9732ca
kharf <kevinfritz210@gmail.com>
2024-07-05 12:31:25 +0200

    feat: remove commit signing with ssh and use gpg instead

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -68,8 +68,6 @@
             insteadOf = "https://github.com/mediamarktsaturn";
          };
         commit.gpgsign = true;
-        gpg.format = "ssh";
-        user.signingkey = "~/.ssh/id_ed25519.pub";
         };
       };
     };
