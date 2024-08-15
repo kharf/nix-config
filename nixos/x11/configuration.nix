afacb14969132dfb88a032c6d8aa853d20de4529
kharf <kevinfritz210@gmail.com>
2024-08-15 09:16:36 +0200

    feat: setup ollama to use gpu

--- nixos/x11/configuration.nix
+++ nixos/x11/configuration.nix
@@ -212,6 +212,10 @@
     enable = true;
     package = pkgs.unstable.ollama;
     acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1030";
+      HSA_OVERRIDE_GFX_VERSION = "10.3.0";
+    };
   };
 
   # signing
